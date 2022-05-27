8795900783e04c196d1d29e5c5e80aa7ee13d64e
Jerry Fadugba <jerryfadugba@gmail.com>
2022-05-27 13:58:31 +0100

    added polyloss

---
+++ polyloss.py
@@ -0,0 +1,33 @@
+""" REF: """
+
+import torch
+from torch.nn.functional import cross_entropy, one_hot, softmax
+
+
+class PolyLoss(torch.nn.Module):
+    """ Implements PolyLoss: A Polynomial Expansion Perspective of Classification Loss Functions
+    <https://arxiv.org/abs/2204.12511>
+
+    for cross_entropy loss
+    """
+
+    def __init__(self, epsilon=2.0, reduction='none', weight=None):
+        super().__init__()
+        self.epsilon = epsilon
+        self.reduction = reduction
+        self.weight = weight
+
+    def forward(self, outputs, targets):
+        ce = cross_entropy(outputs, targets, reduction='none', weight=self.weight)
+        # pt = one_hot(, outputs.size()[1]) * softmax(outputs, 1)
+        onehot_target = one_hot(targets) #, num_classes=self.num_classes
+        
+        pt = torch.sum(onehot_target * softmax(outputs, dim=-1), dim=-1)
+        poly =  ce + self.epsilon * (1.0 - pt)
+        if self.reduction == 'mean':
+            poly = poly.mean()
+        elif self.reduction == 'sum':
+            poly = poly.mean()
+
+        return poly
+
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from csv import writer
 import torch.utils.data as Data
 import numpy as np
 import torch
@@ -7,26 +8,38 @@ from fusion_models.early_fusion import *
 from data import *
 import random
 from sklearn.metrics import classification_report
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+from polyloss import PolyLoss
 
-torch.manual_seed(2021)  # cpu
-torch.cuda.manual_seed(2021)  # gpu
-np.random.seed(2021)  # numpy
-random.seed(2021)  # random and transforms
-torch.backends.cudnn.deterministic = True  
+# torch.manual_seed(2021)  # cpu
+# torch.cuda.manual_seed(2021)  # gpu
+# np.random.seed(2021)  # numpy
+# random.seed(2021)  # random and transforms
+# torch.backends.cudnn.deterministic = True  
 
+writer = SummaryWriter()
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+loss = 'polyloss'  # cross_entropy
 def worker_init_fn(worker_id):
     np.random.seed(2021 + worker_id)
 
 
 batch_size = 16
-num_epochs = 20
-target_names = ['class 0', 'class 1']
-samples_train, labels_train, samples_test, labels_test, num_train_instances, num_test_instances = data_load()
+num_epochs = 5
+target_names = ['Abnormal', 'normal']
+# samples_train, labels_train, samples_test, labels_test, num_train_instances, num_test_instances = data_load()
+
+train_data_loader, test_data_loader, num_train_instances, num_test_instances = data_load()
 
 msresnet = MSResNet(input_channel=1, layers=[1, 1, 1], num_classes=2)
-msresnet = msresnet.cuda()
-criterion = nn.CrossEntropyLoss(reduction='sum').cuda()
+msresnet = msresnet.to(device)
+if loss != 'polyloss':
+    criterion = nn.CrossEntropyLoss(reduction='sum').to(device)
+else:
+    criterion = PolyLoss(reduction='sum').to(device)
 
 optimizer = torch.optim.Adam(msresnet.parameters(), lr=0.0001, weight_decay=0.000002)
 scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[], gamma=0.1)
@@ -36,19 +49,6 @@ train_acc = np.zeros([num_epochs, 1])
 test_acc = np.zeros([num_epochs, 1])
 
 
-# data for training
-dataset_train = Data.TensorDataset(samples_train, labels_train)
-# identify data loader
-dataset_train_loader = Data.DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=True, num_workers=0,
-                                       pin_memory=True, worker_init_fn=worker_init_fn)
-
-
-# data for testing
-dataset_test = Data.TensorDataset(samples_test, labels_test)
-# identify data loader
-dataset_test_loader = Data.DataLoader(dataset=dataset_test, batch_size=60, shuffle=True, num_workers=0,
-                                      pin_memory=True, worker_init_fn=worker_init_fn)
-
 if __name__ == '__main__':
     for epoch in range(num_epochs):
         print('Epoch:', epoch)
@@ -57,28 +57,33 @@ if __name__ == '__main__':
 
         loss_x = 0
         correct_train = 0
-        for i, (samples, labels) in enumerate(dataset_train_loader):
-                samplesV = Variable(samples.cuda())
-                samplesX0 = samplesV[:, 0:1, :, :]
-                samplesX1 = samplesV[:, 1:2, :, :]
-                samplesX2 = samplesV[:, 2:3, :, :]
-                samplesX3 = samplesV[:, 3:4, :, :]
-                samplesX4 = samplesV[:, 4:5, :, :]
-                labels = labels.squeeze()
-                labelsV = Variable(labels.cuda())
+        for i, (samples, labels) in enumerate(train_data_loader):
+                samplesV = Variable(samples.to(device))
+                # samplesX0 = samplesV[:, 0:1, :, :]
+                # samplesX1 = samplesV[:, 1:2, :, :]
+                # samplesX2 = samplesV[:, 2:3, :, :]
+                # samplesX3 = samplesV[:, 3:4, :, :]
+                # samplesX4 = samplesV[:, 4:5, :, :]
+                # labels = labels.squeeze()
+                labelsV = Variable(labels.to(device))
                 optimizer.zero_grad()
-                predict_label = msresnet(samplesX0, samplesX1, samplesX2, samplesX3, samplesX4)
-                prediction = predict_label[0].data.max(1)[1]
+                # predict_label = msresnet(samplesX0, samplesX1, samplesX2, samplesX3, samplesX4)
+                predict_label = msresnet(samplesV)
+                # prediction = predict_label[0].data.max(1)[1]
+                _, prediction = torch.max(predict_label, 1)
                 correct_train += prediction.eq(labelsV.data.long()).sum()
 
-                loss = criterion(predict_label[0], labelsV)
+                loss = criterion(predict_label, labelsV)
                 loss_x += loss.item()
                 loss.backward()
                 optimizer.step()
+        
+        accuracy = 100*float(correct_train)/num_train_instances
+        print("Training accuracy:", accuracy)
+        writer.add_scalar("Loss/train", loss_x / num_train_instances, epoch)
+        writer.add_scalar("Accuracy/train", accuracy, epoch)
 
-        print("Training accuracy:", (100*float(correct_train)/num_train_instances))
-
-        train_loss[epoch] = loss_x / num_train_instances
+        train_loss[epoch] = loss_x / num_train_instances 
         train_acc[epoch] = 100*float(correct_train)/num_train_instances
 
         trainacc = str(100*float(correct_train)/num_train_instances)[0:6]
@@ -91,30 +96,36 @@ if __name__ == '__main__':
         temp_pre = []
 
         msresnet.eval()
-        for i, (samples, labels) in enumerate(dataset_test_loader):
+        for i, (samples, labels) in enumerate(test_data_loader):
             with torch.no_grad():
-                samplesV = Variable(samples.cuda())
-                samplesX0 = samplesV[:, 0:1, :, :]
-                samplesX1 = samplesV[:, 1:2, :, :]
-                samplesX2 = samplesV[:, 2:3, :, :]
-                samplesX3 = samplesV[:, 3:4, :, :]
-                samplesX4 = samplesV[:, 4:5, :, :]
-                labels = labels.squeeze()
-                labelsV = Variable(labels.cuda())
-                predict_label = msresnet(samplesX0, samplesX1, samplesX2, samplesX3, samplesX4)
-                prediction = predict_label[0].data.max(1)[1]
+                samplesV = Variable(samples.to(device))
+                # samplesX0 = samplesV[:, 0:1, :, :]
+                # samplesX1 = samplesV[:, 1:2, :, :]
+                # samplesX2 = samplesV[:, 2:3, :, :]
+                # samplesX3 = samplesV[:, 3:4, :, :]
+                # samplesX4 = samplesV[:, 4:5, :, :]
+                # labels = labels.squeeze()
+                labelsV = Variable(labels.to(device))
+                # predict_label = msresnet(samplesX0, samplesX1, samplesX2, samplesX3, samplesX4)
+                predict_label = msresnet(samplesV)
+                # prediction = predict_label[0].data.max(1)[1]
+                _, prediction = torch.max(predict_label, 1)
+
                 correct_test += prediction.eq(labelsV.data.long()).sum()
                 y_true.extend(labels.cpu().numpy())
                 y_pre.extend(prediction.cpu().numpy())
-                loss = criterion(predict_label[0], labelsV)
+                loss = criterion(predict_label, labelsV)
                 loss_x += loss.item()
 
-        print("Test accuracy:", (100 * float(correct_test) / num_test_instances))
+        acc = (100 * float(correct_test) / num_test_instances)
+        print("Test accuracy:",acc )
 
+        writer.add_scalar("Loss/test", loss_x / num_test_instances, epoch)
+        writer.add_scalar("Accuracy/test", accuracy, epoch)
         test_loss[epoch] = loss_x / num_test_instances
-        test_acc[epoch] = 100 * float(correct_test) / num_test_instances
+        test_acc[epoch] = acc
 
-        testacc = str(100 * float(correct_test) / num_test_instances)[0:6]
+        testacc = str(acc)[0:6]
 
         if epoch == 0:
             temp_test = correct_test
@@ -140,7 +151,7 @@ if __name__ == '__main__':
     # sio.savemat('result/changingResnet/TestAccuracy_' + 'ChangingSpeed_Train' + str(100*float(temp_train)/num_train_instances)[0:6] + 'Test' + str(100*float(temp_test)/num_test_instances)[0:6] + '.mat', {'test_acc': test_acc})
     # print(str(float(temp_test)/num_test_instances)[0:6])
     # print(str(float(correct_test)/num_test_instances)[0:6])
-    #
+    
     # plt.plot(train_loss)
     # plt.show()
 
